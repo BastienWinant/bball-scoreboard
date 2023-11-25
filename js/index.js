b1cb1afa2c8e2caef4ee26c734f934c142e8a0e1
Armand Winant <145713109+BastienWinant@users.noreply.github.com>
2023-11-25 23:50:44 +0100

    add and apply function to update score cards

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
+const homeTeam = {
+	designation: "home",
+	points: 4,
+	fouls: 5,
+	timeouts: 6
+};
+
+const awayTeam = {
+	designation: "away",
+	points: 0,
+	fouls: 5,
+	timeouts: 6
+};
+
+const teams = [homeTeam, awayTeam];
+
+function updateScoreCards() {
+	let scoreCards = document.getElementsByClassName("pts-card");
+
+	for (let i = 0; i < 2; i++) {
+		scoreCards[i].textContent = teams[i].points;
+	}
+};
+
 document.addEventListener("DOMContentLoaded", () => {
-	console.log("Hello World");
-})
+	updateScoreCards();
+});
