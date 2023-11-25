8ee8071a542b7f92e5f1a14cb6f5c09a44da23bc
Armand Winant <145713109+BastienWinant@users.noreply.github.com>
2023-11-26 00:51:58 +0100

    add comments

--- js/index.js
+++ js/index.js
@@ -17,15 +17,19 @@ const teams = [homeTeam, awayTeam];
 const scoreCards = document.getElementsByClassName("pts-card");
 
 function addPoints(teamIndex, points) {
+	// add the score in the appropriate team object
 	teams[teamIndex].points += points;
+
+	// update the html cards content
 	updateScoreCards();
 }
 
 function updateScoreCards() {
+	// pull the data in the team objects to update html
 	for (let i = 0; i < 2; i++) {
 		scoreCards[i].textContent = teams[i].points;
 	}
-};
+}; 
 
 document.addEventListener("DOMContentLoaded", () => {
 	updateScoreCards();
