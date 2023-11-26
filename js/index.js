0b34dfa557584b730305d293c914fc66c148a423
Armand Winant <145713109+BastienWinant@users.noreply.github.com>
2023-11-26 01:12:08 +0100

    create resetting function

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const homeTeam = {
 	designation: "home",
-	points: 4,
+	points: 0,
 	fouls: 5,
 	timeouts: 6
 };
@@ -24,10 +24,37 @@ function addPoints(teamIndex, points) {
 	updateScoreCards();
 }
 
+function resetScore() {
+	homeTeam.points = 0;
+	awayTeam.points = 0;
+
+	homeTeam.fouls = 5;
+	awayTeam.fouls = 5;
+
+	homeTeam.timeouts = 6;
+	awayTeam.timeouts = 6;
+
+	updateScoreCards();
+}
+
 function updateScoreCards() {
 	// pull the data in the team objects to update html
 	for (let i = 0; i < 2; i++) {
 		scoreCards[i].textContent = teams[i].points;
+	}
+
+	const homePoints = homeTeam.points;
+	const awayPoints = awayTeam.points;
+
+	if (homePoints > awayPoints) {
+		scoreCards[0].style.color = "red";
+		scoreCards[1].style.color = "black";
+	} else if (homePoints < awayPoints) {
+		scoreCards[0].style.color = "black";
+		scoreCards[1].style.color = "red";
+	} else {
+		scoreCards[0].style.color = "black";
+		scoreCards[1].style.color = "black";
 	}
 }; 
 
